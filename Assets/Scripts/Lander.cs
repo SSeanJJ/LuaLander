77e2487b08ad329aa8404493b13c443e9335169f
IamLi <Seanjaeger188@gmail.com>
2026-08-29 17:52:22 -0500

    Created Landing Pad, and implemented landing pad multiplier. Text dynamically changes based of multiplier on landing pad. also fully implemented landing detection.

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -1,3 +1,4 @@
+using Unity.Burst;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,6 +8,12 @@ public class Lander : MonoBehaviour
     private void Awake()
     {
         landerRigidbody2D = GetComponent<Rigidbody2D>();
+
+        Debug.Log(Vector2.Dot(new Vector2(0, 1), new Vector2(0, 1)));
+        Debug.Log(Vector2.Dot(new Vector2(0, 1), new Vector2(.5f, .5f)));
+        Debug.Log(Vector2.Dot(new Vector2(0, 1), new Vector2(1,0)));
+        Debug.Log(Vector2.Dot(new Vector2(0, 1), new Vector2(0, -1)));
+
     }
 
     private void FixedUpdate()
@@ -27,6 +34,50 @@ public class Lander : MonoBehaviour
             landerRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        if (!collision2D.gameObject.TryGetComponent(out LandingPad landingPad))
+        {
+            Debug.Log("Crash!");
+            return;
+        }
+
+        float softLandingVelocityMagnitude = 4f;
+        float relativeVelocityMagnitude = collision2D.relativeVelocity.magnitude;
+        if (relativeVelocityMagnitude > softLandingVelocityMagnitude)
+        {
+            //Landed Too Hard!
+            Debug.Log("Landed Too Hard!");
+            return;
+        }
+
+        float dotVector = Vector2.Dot(Vector2.up, transform.up);
+        float minDotVector = .90f;
+        if (dotVector < minDotVector) {
+            // Landed on a too steep angle!
+            Debug.Log("Landed on a too steep angle!");
+            return;
+        }
+
+        Debug.Log("Successful Landing!");
+
+        float maxScoreAmountLandingAngle = 100;
+        float scoreDotVectorMultiplier = 10f;
+        float landingAngleScore = maxScoreAmountLandingAngle - Mathf.Abs(dotVector - 1f) * scoreDotVectorMultiplier *maxScoreAmountLandingAngle;
+
+        float maxScoreAmountLandingSpeed = 100;
+        float landingSpeedScore = (softLandingVelocityMagnitude - relativeVelocityMagnitude) * maxScoreAmountLandingSpeed;
+
+        Debug.Log("LandingAngleScore: " + landingAngleScore);
+        Debug.Log("landingSpeedScore" + landingSpeedScore);
+
+        int score = Mathf.RoundToInt((landingAngleScore + landingSpeedScore) * landingPad.GetScoreMultiplier());
+
+        Debug.Log("Final Score:" + score);
+    
+    }
+
 
 }
 
