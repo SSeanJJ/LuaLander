01126d76aea7e721696a02c328a3282b33757782
IamLi <Seanjaeger188@gmail.com>
2026-08-29 18:51:03 -0500

    Implemented thrust visual logic.

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -1,9 +1,15 @@
+using System;
 using Unity.Burst;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Lander : MonoBehaviour
 {
+    public event EventHandler OnUpForce;
+    public event EventHandler OnRightForce;
+    public event EventHandler OnLeftForce;
+
+    public event EventHandler OnBeforeForce;
     private Rigidbody2D landerRigidbody2D;
     private void Awake()
     {
@@ -16,22 +22,27 @@ public class Lander : MonoBehaviour
 
     }
 
-    private void FixedUpdate()
-    {
+    private void FixedUpdate() {
+        OnBeforeForce?.Invoke(this, EventArgs.Empty);
         if (Keyboard.current.upArrowKey.isPressed)
         {
             float force = 700f;
             landerRigidbody2D.AddForce(force * transform.up * Time.deltaTime);
+            OnUpForce?.Invoke(this, EventArgs.Empty);
         }
         if (Keyboard.current.leftArrowKey.isPressed)
         {
             float turnSpeed = +100f;
             landerRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
+            OnLeftForce?.Invoke(this, EventArgs.Empty);
+
         }
         if (Keyboard.current.rightArrowKey.isPressed)
         {
             float turnSpeed = -100f;
             landerRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
+            OnRightForce?.Invoke(this, EventArgs.Empty);
+
         }
     }
 
