cbd9e4dc52e03d6fcaa566d2830721644b75c577
IamLi <Seanjaeger188@gmail.com>
2026-08-31 14:21:44 -0500

    Implemented Coin pickup, and created game manager script.

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -5,28 +5,38 @@ using UnityEngine.InputSystem;
 
 public class Lander : MonoBehaviour
 {
+    public static Lander Instance { get; private set; }
     public event EventHandler OnUpForce;
     public event EventHandler OnRightForce;
     public event EventHandler OnLeftForce;
 
     public event EventHandler OnBeforeForce;
+
+    public event EventHandler OnCoinPickup;
+    public event EventHandler<OnLandedEventArgs> OnLanded;
+    public class OnLandedEventArgs : EventArgs
+    {
+        public int score; 
+    }
     private Rigidbody2D landerRigidbody2D;
     private float fuelAmount = 10f;
     private void Awake()
     {
+        Instance = this;
+
         landerRigidbody2D = GetComponent<Rigidbody2D>();
 
-        Debug.Log(Vector2.Dot(new Vector2(0, 1), new Vector2(0, 1)));
-        Debug.Log(Vector2.Dot(new Vector2(0, 1), new Vector2(.5f, .5f)));
-        Debug.Log(Vector2.Dot(new Vector2(0, 1), new Vector2(1,0)));
-        Debug.Log(Vector2.Dot(new Vector2(0, 1), new Vector2(0, -1)));
+       // Debug.Log(Vector2.Dot(new Vector2(0, 1), new Vector2(0, 1)));
+       // Debug.Log(Vector2.Dot(new Vector2(0, 1), new Vector2(.5f, .5f)));
+       // Debug.Log(Vector2.Dot(new Vector2(0, 1), new Vector2(1,0)));
+       // Debug.Log(Vector2.Dot(new Vector2(0, 1), new Vector2(0, -1)));
 
     }
 
     private void FixedUpdate() {
         OnBeforeForce?.Invoke(this, EventArgs.Empty);
 
-        Debug.Log(fuelAmount);
+       // Debug.Log(fuelAmount);
         if(fuelAmount <= 0f)
         {
             //no fuel
@@ -93,7 +103,7 @@ public class Lander : MonoBehaviour
 
         float maxScoreAmountLandingAngle = 100;
         float scoreDotVectorMultiplier = 10f;
-        float landingAngleScore = maxScoreAmountLandingAngle - Mathf.Abs(dotVector - 1f) * scoreDotVectorMultiplier *maxScoreAmountLandingAngle;
+        float landingAngleScore = maxScoreAmountLandingAngle - Mathf.Abs(dotVector - 1f) * scoreDotVectorMultiplier * maxScoreAmountLandingAngle;
 
         float maxScoreAmountLandingSpeed = 100;
         float landingSpeedScore = (softLandingVelocityMagnitude - relativeVelocityMagnitude) * maxScoreAmountLandingSpeed;
@@ -104,6 +114,10 @@ public class Lander : MonoBehaviour
         int score = Mathf.RoundToInt((landingAngleScore + landingSpeedScore) * landingPad.GetScoreMultiplier());
 
         Debug.Log("Final Score:" + score);
+        OnLanded?.Invoke(this, new OnLandedEventArgs
+        {
+            score = score,
+        });
     
     }
 
@@ -114,6 +128,12 @@ public class Lander : MonoBehaviour
             float addFuelAmount = 10f;
             fuelAmount += addFuelAmount;
             fuelPickup.DestroySelf();
+        }
+
+        if (collider2D.gameObject.TryGetComponent(out CoinPickup coinPickup))
+        {
+            OnCoinPickup?.Invoke(this, EventArgs.Empty);
+            coinPickup.DestroySelf();
         }
     }
 
