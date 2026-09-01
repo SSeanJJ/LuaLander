38deb1940cc4c0bd03d79d69f29246365812119c
IamLi <Seanjaeger188@gmail.com>
2026-09-01 16:05:28 -0500

    Created Level prefabs and the implemented the ability to progress through levels based off success. (Need to fix Stats UI)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,61 +1,78 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
 
 
     public static GameManager Instance { get; private set; }
+    private static int levelNumber = 1;
+    [SerializeField] private List<GameLevel> gameLevelList;
+
     private int score;
     private float time;
     private bool isTimerActive;
 
-    private void Awake()
-    {
+    private void Awake() {
         Instance = this;
     }
-    private void Start()
-    {
+    private void Start() {
         Lander.Instance.OnCoinPickup += Lander_OnCoinPickup;
         Lander.Instance.OnLanded += Lander_OnLanded;
         Lander.Instance.OnStateChanged += Lander_OnStateChanged;
+
+        loadCurrentLevel();
     }
-    private void Lander_OnStateChanged(object sender, Lander.OnStateChangedEventArgs e)
-    {
+    private void Lander_OnStateChanged(object sender, Lander.OnStateChangedEventArgs e) {
         isTimerActive = e.state == Lander.State.Normal;
     }
-    private void Update()
-    {
-        if (isTimerActive)
-        {
-          time += Time.deltaTime;
+
+    private void loadCurrentLevel() {
+        foreach (GameLevel gameLevel in gameLevelList) {
+            if (gameLevel.GetLevelNumber() == levelNumber) {
+                GameLevel spawnedGameLevel = Instantiate(gameLevel, Vector3.zero, Quaternion.identity);
+                Lander.Instance.transform.position = spawnedGameLevel.GetLanderStartPosition();
+            }
+        }
+    }
+    private void Update() {
+        if (isTimerActive) {
+            time += Time.deltaTime;
         }
     }
 
-    private void Lander_OnLanded(object sender, Lander.OnLandedEventArgs e)
-    {
+    private void Lander_OnLanded(object sender, Lander.OnLandedEventArgs e) {
         AddScore(e.score);
     }
 
-    private void Lander_OnCoinPickup(object sender, System.EventArgs e)
-    {
+    private void Lander_OnCoinPickup(object sender, System.EventArgs e) {
         AddScore(500);
     }
 
-    public void AddScore(int addScoreAmount)
-    {
+    public void AddScore(int addScoreAmount) {
         score += addScoreAmount;
         Debug.Log(score);
     }
 
-    public int GetScore()
-    {
+    public int GetScore() {
         return score;
     }
 
-    public float GetTime()
-    {
+    public float GetTime() {
         return time;
     }
+
+    public void GoToNextLevel() {
+        levelNumber++;
+        SceneManager.LoadScene(0);
+    }
+
+    public void RetryLevel() {
+        SceneManager.LoadScene(0);
+    }
+
 
 }
 
--- Assets/Scripts/LandedUI.cs
+++ Assets/Scripts/LandedUI.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,14 +8,17 @@ public class LandedUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI titleTextMesh;
     [SerializeField] private TextMeshProUGUI statsTextMesh;
+    [SerializeField] private TextMeshProUGUI nextButtonTextMesh;
     [SerializeField] private Button nextButton;
 
+
+    private Action nextButtonClickAction;
 
     private void Awake()
     {
         nextButton.onClick.AddListener(() =>
         {
-            SceneManager.LoadScene(0);
+            nextButtonClickAction();
         });
     }
 
@@ -28,9 +32,13 @@ public class LandedUI : MonoBehaviour
     private void Lander_OnLanded(object sender, Lander.OnLandedEventArgs e)
     {
         if (e.landingType == Lander.LandingType.Success) {
-            titleTextMesh.text = "SUCCESSFUL LANDING!"; 
+            titleTextMesh.text = "SUCCESSFUL LANDING!";
+            nextButtonTextMesh.text = "CONTINUE";
+            nextButtonClickAction = GameManager.Instance.GoToNextLevel; 
         } else { 
             titleTextMesh.text = "<color=#ff0000>CRASH!</color>";
+            nextButtonTextMesh.text = "RETRY";
+            nextButtonClickAction = GameManager.Instance.RetryLevel;
         }
 
         statsTextMesh.text =
